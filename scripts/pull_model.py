d5292646737d004ca0f12083962b6af291d446c3
Niko <n.vlahovic@proton.me>
2024-01-29 22:48:38 +0100

    updated docker script(s)

---
+++ scripts/pull_model.py
@@ -0,0 +1,36 @@
+import argparse
+import subprocess
+
+
+LLM_SERVICE_NAME: str = 'ollama'
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--model',
+    '-m',
+    required=True,
+    type=str,
+    help='The model for ollama to pull.',
+)
+args = parser.parse_args()
+
+
+def pull_model(model: str) -> None:
+    subprocess.run([
+        'docker',
+        'exec',
+        '-it',
+        LLM_SERVICE_NAME,
+        'ollaman',
+        'pull',
+        model,
+    ])
+
+
+def main() -> None:
+    model = args.model
+    pull_model(model)
+
+
+if __name__ == '__main__':
+    main()
